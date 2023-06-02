bf8e82d0446f79d39b7475298f550350f99ef355
Tai Tran <transytai12a1@gmail.com>
2023-06-02 15:16:26 +0700

    Handle todo data layer

--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -10,3 +10,99 @@ const XAWS = AWSXRay.captureAWS(AWS)
 const logger = createLogger('TodosAccess')
 
 // TODO: Implement the dataLayer logic
+export class TodoAccess {
+  constructor(
+    private readonly docClient: DocumentClient = new XAWS.DynamoDB.DocumentClient(),
+    private readonly todosTable = process.env.TODOS_TABLE
+  ) {}
+
+  async getTodos(userId: string): Promise<TodoItem[]> {
+    logger.info('Get all todo items')
+    const result = await this.docClient.query({
+      TableName: this.todosTable,
+      KeyConditionExpression: 'userId = :userId',
+      ExpressionAttributeValues: {
+        ':userId': userId
+      }
+    }).promise()
+
+    return result.Items as TodoItem[]
+  }
+
+  async getTodo(userId: string, todoId: string): Promise<TodoItem> {
+    logger.info('Get todo item ${todoId}')
+    const result = await this.docClient.query({
+      TableName: this.todosTable,
+      KeyConditionExpression: 'userId = :userId and todoId = :todoId',
+      ExpressionAttributeValues: {
+        ':userId': userId,
+        ':todoId': todoId
+      }
+    }).promise()
+
+    return result.Items[0] as TodoItem
+  }
+
+  async createTodo(todoItem: TodoItem): Promise<TodoItem> {
+    logger.info('Create a new todo item')
+    const result = await this.docClient.put({
+      TableName: this.todosTable,
+      Item: todoItem
+    }).promise()
+
+    logger.info(result)
+
+    return todoItem
+  }
+
+  async updateTodo(userId: string, todoId: string, todoData: TodoUpdate): Promise<TodoUpdate> {
+    logger.info('Update a todo item ${todoId}')
+    const result = await this.docClient.update({
+      TableName: this.todosTable,
+      Key: { userId, todoId },
+      ConditionExpression: 'attribute_exists(todoId)',
+      UpdateExpression: 'set #name = :name, dueDate = :dueDate, done =:done',
+      ExpressionAttributeNames: {
+        '#name': 'name' // Reserved words
+      },
+      ExpressionAttributeValues: {
+        ':name': todoData.name,
+        ':dueDate': todoData.dueDate,
+        ':done': todoData.done
+      }
+    }).promise()
+
+    logger.info(result)
+
+    return todoData
+  }
+
+  async deleteTodo(userId: string, todoId: string): Promise<string> {
+    logger.info('Delete a todo item ${todoId}')
+    const result = await this.docClient.delete({
+      TableName: this.todosTable,
+      Key: {userId, todoId }
+    }).promise()
+
+    logger.info(result)
+
+    return todoId
+  }
+
+  async updateAttachmentUrl(userId: string, todoId: string, attachmentUrl: string): Promise<string> {
+    logger.info('Update a todo item attachment url ${todoId}')
+    const result = await this.docClient.update({
+      TableName: this.todosTable,
+      Key: {userId, todoId }
+      ConditionExpression: 'attribute_exists(todoId)',
+      UpdateExpression: 'set attachmentUrl = :attachmentUrl',
+      ExpressionAttributeValues: {
+        ':attachmentUrl': attachmentUrl
+      }
+    }).promise()
+
+    logger.info(result)
+
+    return attachmentUrl
+  }
+}
