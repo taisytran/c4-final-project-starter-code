caaee693f2b4c7acb2e61a0247c635112e10968b
Tai Tran <transytai12a1@gmail.com>
2023-06-02 16:27:38 +0700

    Handle deleteTodo

--- backend/src/lambda/http/deleteTodo.ts
+++ backend/src/lambda/http/deleteTodo.ts
@@ -4,15 +4,26 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { deleteTodo } from '../../businessLogic/todos'
+import { deleteTodo } from '../../helpers/todos'
 import { getUserId } from '../utils'
 
 export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
-    // TODO: Remove a TODO item by id
-    
-    return undefined
+    // TODO(done): Remove a TODO item by id
+    const userId = getUserId(event)
+    const result = deleteTodo(userId, todoId)
+
+    logger.info(result)
+
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: `Todo item #${todoId} deleted successfully`
+    }
   }
 )
 
